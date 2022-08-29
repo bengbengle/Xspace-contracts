5c06c214282163314a143ddb773afcbdf7ff863c
Terry <43799882+bengbengle@users.noreply.github.com>
2022-08-29 12:38:49 +0800

    test

--- test/activity/Activity.test.ts
+++ test/activity/Activity.test.ts
@@ -19,30 +19,25 @@ type AddressData = {
   tokenAmount: number;
 };
 
-const init_tree = (
-  distributor: string,
-  _id: number,
-  tokenAddress: string,
-  list: string[]
-) => {
-  let _tree = {
-    distributor: distributor,
-    activityId: _id,
-    tokenAddress,
-    addresses: [
-      {
-        recipient: list[0],
-        tokenAddress,
-        tokenAmount: 1,
-      },
-      {
-        recipient: list[1],
-        tokenAddress,
-        tokenAmount: 2
-      },
-    ],
-  };
-  return _tree;
+const recipient_list = (list: string[], tokenAddress: string) => {
+  return [
+    {
+      // 领奖者地址
+      recipient: list[0],
+      // erc20 代币地址
+      tokenAddress,
+      // 领奖金额
+      tokenAmount: 1,
+    },
+    {
+      // 领奖者地址
+      recipient: list[1],
+      // erc20 代币地址
+      tokenAddress,
+      // erc20 领奖金额
+      tokenAmount: 2
+    },
+  ];
 };
 
 describe("Activity", () => {
@@ -53,16 +48,20 @@ describe("Activity", () => {
 
   let signers: SignerWithAddress[];
 
-  let ownerAddress: string;
+  let distributor: string;
 
   let addresses: AddressData[];
 
   let tree: MerkleTree;
 
+  let _list: string[];
+  let _recipient_list: AddressData[];
+
   beforeEach(async () => {
     signers = await ethers.getSigners();
 
-    ownerAddress = "0xa8E7813150a988e7F20193983fA3017155F3C162";
+    // 活动发起者
+    distributor = "0xa8E7813150a988e7F20193983fA3017155F3C162";
 
     activity = await new Activity__factory(signers[0]).deploy();
     token = await new Token__factory(signers[0]).deploy();
@@ -71,51 +70,43 @@ describe("Activity", () => {
   it("Activity Create", async () => {
 
     let tokenAddress = '0x2441AC1a15cB95B34766913E660f423F267e0E70'
-    let _id = 1;
-    let list = ['0xa8E7813150a988e7F20193983fA3017155F3C162', '0x7009E5c74B3b3f28e622909e5A987e6F0e915cB7'];
-    let distributor = "0xa8E7813150a988e7F20193983fA3017155F3C162";
     
-    const _tree = init_tree(distributor, _id, tokenAddress, list);
+    // 领奖者
+    _list = [
+      '0xa8E7813150a988e7F20193983fA3017155F3C162', 
+      '0x7009E5c74B3b3f28e622909e5A987e6F0e915cB7'
+    ];
 
-    tree = new MerkleTree(
-      distributor,
-      _id,
-      token.address,
-      _tree.addresses
-    );
+    // ----- 通过监听 created 事件 获取
 
+    // // 活动发起者
+    // let distributor = "0xa8E7813150a988e7F20193983fA3017155F3C162";
+    
+    // // 活动ID
+    // let _id = 1;
 
-    const rootHash = tree.getRoot();
+    // const _recipient_list = recipient_list(distributor, _id, tokenAddress, list);
 
-    let leaves = tree.expandLeaves();
-    let leaf = leaves.find(
-      (leaf) =>
-        leaf.recipient.toLowerCase() === list[1].toLowerCase()
-    );
+    // tree = new MerkleTree(distributor, _id, token.address, _recipient_list);
 
-    if (!leaf) return;
-    let index = leaf.index;
-    let proofs = tree.getProof(index);
-
-    console.log("addresses:");
-    console.log("Merkle tree root hash:", rootHash);
-    console.log("proofs", proofs);
-    console.log("index", index);
-    console.log("recipient", leaf.recipient);
- 
   });
 
   it("Activity Start", async () => {
-    ownerAddress = signers[0].address;
+    
+    distributor = signers[0].address;
     await token.approve(activity.address, 10);
     await activity.create(token.address, 10);
 
-    let activity_id = await activity.activityIds(ownerAddress);
+    // 最新 活动 Id
+    let activity_id = await activity.activityIds(distributor);
+
     console.log("activity_id", activity_id.toNumber());
 
-    const _tree = init_tree(ownerAddress, activity_id.toNumber(), token.address, [signers[0].address, signers[1].address]);
+    // Mock 领奖者 列表
+    _recipient_list = recipient_list(_list, token.address);
 
-    tree = new MerkleTree(ownerAddress, activity_id.toNumber(), token.address, _tree.addresses);
+    // 生成 MerkleTree rootHash
+    tree = new MerkleTree(distributor, activity_id.toNumber(), token.address, _recipient_list);
 
     await activity.start(activity_id.toNumber(), tree.getRoot());
     
@@ -126,7 +117,29 @@ describe("Activity", () => {
     let balance = await token.balanceOf(signers[0].address);
     console.log("balance.toNumber():", balance.toNumber());
 
-    await activity.claim(ownerAddress, activity_id.toNumber(), 1, signers[1].address, token.address, 2, tree.getProof(1));
+    // 最新 活动 Id
+    let activity_id = await activity.activityIds(distributor);
+
+
+    // 获取用户的 Index
+    let leaves = tree.expandLeaves();
+    let leaf = leaves.find(
+      (leaf) =>
+        leaf.recipient.toLowerCase() === _list[1].toLowerCase()
+    );
+    if (!leaf) return;
+    let index = leaf.index;
+
+
+    await activity.claim(
+      distributor, 
+      activity_id.toNumber(), 
+      index, 
+      leaf.recipient, 
+      leaf.tokenAddress, 
+      leaf.tokenAmount, 
+      tree.getProof(leaf.index)
+    );
 
     balance = await token.balanceOf(signers[0].address);
     console.log("balance.toNumber():", balance.toNumber());
